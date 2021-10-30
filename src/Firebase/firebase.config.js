2bb90e13ee23047c1dc220242b4e44341aac5c65
Mhedii <mehedi1234.mh@gmail.com>
2021-10-30 08:52:33 +0600

    try

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBPSFzSK_LI0daVMDOJiWAg-D2JSi_Cuiw",
+    authDomain: "tour-planner-17a35.firebaseapp.com",
+    projectId: "tour-planner-17a35",
+    storageBucket: "tour-planner-17a35.appspot.com",
+    messagingSenderId: "318492999032",
+    appId: "1:318492999032:web:3a91ae10090595b8fcdc8d"
+};
+
+
+export default firebaseConfig;
